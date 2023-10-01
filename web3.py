571d12285d7c5bafd3baecfe6591728bf76dce56
neelkothimbire <106696761+neelkothimbire@users.noreply.github.com>
2023-10-01 12:55:00 +0530

    Add files via upload
    Without creating a external file direct output in the terminal

--- web3.py
+++ web3.py
@@ -16,7 +16,7 @@ def find_jobs():
     jobs = soup.find_all('li', class_='clearfix job-bx wht-shd-bx')
 
     # Iterate through each job
-    for index, job in enumerate(jobs):
+    for job in jobs:
         job_published_date = job.find('span', class_='sim-posted').span.text
         if 'few' in job_published_date:
             company_name = job.find('h3', class_='joblist-comp-name').text.replace(' ', '')  # Remove white spaces
@@ -25,11 +25,10 @@ def find_jobs():
             
             # Check if the unfamiliar skill is not required for the job
             if unfamiliar_skill not in skills:
-                with open(f'posts/{index}.txt', 'a') as f:
-                    f.write(f"Company Name: {company_name.strip()}\n")
-                    f.write(f"Required skills: {skills.strip()}\n")
-                    f.write(f"Link: {more_info.strip()}\n")
-                print(f'File Saved: {index}')
+                print(f"Company Name: {company_name.strip()}\n")
+                print(f"Required skills: {skills.strip()}\n")
+                print(f"Link: {more_info.strip()}\n")
+                print(' ')
 
 if __name__ == '__main__':
     while True:
